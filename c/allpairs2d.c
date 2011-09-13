91eea6a75fa5c153d83b0413f5da3c32b04434c2
Nan Dun <Nan Dun@localhost>
2011-09-13 18:08:36 +0900

    .

--- c/allpairs2d.c
+++ c/allpairs2d.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright (C) 2011  Nan Dun <dunnan@yl.is.s.u-tokyo.ac.jp>
+ * Copyright (C) 2011  Nan Dun <dun@logos.ic.i.u-tokyo.ac.jp>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,15 +19,91 @@
 
 /* allpairs2d.c */
 
-#include <stdlib.h>
-#include <stdio.h>
+#define DIMS	2	/* Our system is 2-dimensional */
 
-int main(int argc, char *argv[])
+#include "common.h"
+
+float MY_VERSION = 0.1;
+int MY_DEBUG = 0;
+
+struct atom {
+	struct vec r;	/* coordinates */
+	struct vec rv;	/* velocity */
+	struct vec ra;	/* acceleration */
+};
+
+struct md {
+	double rCut;		/* distance threshold */
+	struct vec region;
+	unsigned int N;		/* # of atoms */
+	double initVel;		/* initial velocity */
+	struct atom *mol;	/* array of atoms */
+	struct vec velSum;	/* velocity sum */
+	unsigned int stepCount;
+	
+	/* variables from config */
+	double deltaT;
+	double density;
+	double temperature;
+	struct veci initUcell;
+	int stepAvg;
+	int stepEquil;
+	int stepLimit;
+};
+
+void init(struct md *md, struct config *cfg)
 {
+	DEBUG("Init: system dimensionality = %d\n", DIMS);
+	md->deltaT = cfg->deltaT;
+	md->density = cfg->density;
+	md->temperature = cfg->temperature;
+	md->initUcell = cfg->initUcell;
+	md->stepAvg = cfg->stepAvg;
+	md->stepEquil = cfg->stepEquil;
+	md->stepLimit = cfg->stepLimit;
+
+	md->rCut = pow(2., 1./6.);
+	vSCopy(md->region, 1./sqrt(md->density), md->initUcell);
+	md->N = vProd(md->initUcell);
+	md->initVel = sqrt(DIMS * (1. - 1. / md->N) * md->temperature);
+	
+	md->mol = (struct atom *) (intptr_t) xmalloc(
+		md->N * sizeof(struct atom));
+	md->stepCount = 0;
+
+	/* Initial coordinates */
+	struct vec c, gap;
+	unsigned int nx, ny, n;
+	vDiv(gap, md->region, md->initUcell);
+	for (ny = 0; ny < md->initUcell.y; ny++) {
+		for (nx = 0; nx < md->initUcell.x; nx++) {
+			vSet(c, nx + 0.5, ny + 0.5);	/* center of box of lattice */
+			vMul(c, c, gap);
+			vSAdd(c, c, -0.5, md->region);
+			md->mol[n].r = c;
+			n++;
+		}
+	}
+
+	/* Initial velocities */
+	vSet(md->velSum, 0, 0);
+	for (n = 0; n < md->N; n++) {
+	}
+
+}
+
+int main(int argc, char **argv)
+{
+	struct config cfg;
+	struct md md;
 	int steps = 0;
 	int steps_limit = 10;
 	
-	/* Initilization */
+	cmd_opts_t opts = cmd_parsing(argc, argv);
+	config_parsing(&cfg, opts);
+	cmd_free_opts(opts);
+	
+	init(&md, &cfg);	
 
 	while (steps < steps_limit) {
 		printf("Conduct step %d\n", steps);
---
+++ c/common.c
@@ -0,0 +1,180 @@
+/****************************************************************************
+ * Copyright (C) 2011  Nan Dun <dun@logos.ic.i.u-tokyo.ac.jp>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program can be distributed under the terms of the GNU GPL.
+ * See the file COPYING.
+ ***************************************************************************/
+
+/* common.c */
+
+#include "common.h"
+
+extern float MY_VERSION;
+extern int MY_DEBUG;
+
+void fatal(const char *str)
+{
+	fprintf(stderr, "fatal error: %s\n", str);
+	abort();
+}
+
+void * xmalloc(size_t size)
+{
+	register void *value = malloc(size);
+	if (value == NULL)
+		fatal("failed to allocate memory");
+	return value;
+}
+
+static void perror2(const char *format, ...)
+{
+	va_list argv;
+    fflush(stdout);
+    fprintf(stderr, "error: ");
+    va_start(argv, format);
+	vfprintf(stderr, format, argv);
+	va_end(argv);
+    fprintf(stderr, ": %s\n", strerror(errno));
+}
+
+
+void usage(const char *progname)
+{
+	printf(
+"Usage: %s [OPTION...]\n"
+"\n"
+"    -i                input file\n"
+"    -h                print help\n"
+"    -v                print version\n"
+"    -d                print debug information\n"
+"Report bugs to dun@logos.ic.i.u-tokyo.ac.jp\n"
+"For more information: <http://mdoch.googlecode.com/>\n"
+	, progname);
+}
+
+void version(void)
+{
+	printf(
+"Version %.1f\n"
+"Report bugs to dun@logos.ic.i.u-tokyo.ac.jp\n"
+"For more information: <http://mdoch.googlecode.com/>\n"
+	, MY_VERSION);
+}
+
+void cmd_free_opts(struct cmd_opts *opts)
+{
+	if (opts != NULL) {
+		free(opts->in_file);
+		free(opts);
+	}
+}
+
+struct cmd_opts * cmd_parsing(int argc, char **argv)
+{	
+	int o;
+	cmd_opts_t opts = NULL;
+
+	if (argc == 1) {
+		usage(argv[0]);
+		exit(0);
+	}
+	
+	opts = xmalloc(sizeof(struct cmd_opts));
+	memset(opts, 0x0, sizeof(struct cmd_opts));
+	while ((o = getopt(argc, argv, "dhi:v")) != -1) {
+		switch (o) {
+			case 'd':	/* debugging */
+				MY_DEBUG = 1;
+				break;
+			case 'h':	/* print usage */
+				cmd_free_opts(opts);
+				usage(argv[0]);
+				exit(0);
+			case 'i':	/* input file */
+				opts->in_file = strdup(optarg);
+				break;
+			case 'v':
+				cmd_free_opts(opts);
+				version();
+				exit(0);
+			case '?':
+				cmd_free_opts(opts);
+				exit(1);
+			default:
+				cmd_free_opts(opts);
+				abort();
+		}
+	}
+	return opts;
+}
+
+int config_parsing(struct config *cfg, struct cmd_opts *opts)
+{
+	char buf[LINE_MAX], *item, *value;
+	const char *delimiters = " \t\n";
+	FILE *fp;
+	int line_no = 0;
+
+	fp = fopen(opts->in_file, "r");
+	if (fp == NULL) {
+		perror(opts->in_file);
+		exit(1);
+	}
+
+	DEBUG("Load configuration from %s ...\n", opts->in_file);
+	memset(buf, 0x0, LINE_MAX);
+	while (fgets(buf, LINE_MAX, fp) != NULL) {
+		line_no++;
+		item = strtok(buf, delimiters);
+		value = strtok(NULL, delimiters);
+		
+		/* assign item/value to configurations */
+		if (strcmp(item, "deltaT") == 0) {
+			cfg->deltaT = atof(value);
+			DEBUG(" line %d: deltaT = %f\n", line_no, cfg->deltaT);
+		} else if (strcmp(item, "density") == 0) {
+			cfg->density = atof(value);
+			DEBUG(" line %d: density = %f\n", line_no, cfg->density);
+		} else if (strcmp(item, "temperature") == 0) {
+			cfg->temperature = atof(value);
+			DEBUG(" line %d: temperature = %f\n", line_no, cfg->temperature);
+		} else if (strcmp(item, "initUcell") == 0) {
+			cfg->initUcell.x = atoi(value);
+			value = strtok(NULL, delimiters);
+			cfg->initUcell.y = atoi(value);
+#if DIMS == 3
+			value = strtok(NULL, delimiters);
+			cfg->initUcell.z = atoi(value);
+#endif
+#if DIMS == 2
+			DEBUG(" line %d: initUcell = (%d, %d) \n", 
+				line_no, cfg->initUcell.x, cfg->initUcell.y);
+#elif DIMS == 3
+			DEBUG(" line %d: initUcell = (%d, %d, %d) \n",  line_no, 
+				cfg->initUcell.x, cfg->initUcell.y, cfg->initUcell.z);
+#endif
+		} else if (strcmp(item, "stepAvg") == 0) {
+			cfg->stepAvg = atoi(value);
+			DEBUG(" line %d: stepAvg = %d\n", line_no, cfg->stepAvg);
+		} else if (strcmp(item, "stepEquil") == 0) {
+			cfg->stepEquil = atoi(value);
+			DEBUG(" line %d: stepEquil = %d\n", line_no, cfg->stepEquil);
+		} else if (strcmp(item, "stepLimit") == 0) {
+			cfg->stepLimit = atoi(value);
+			DEBUG(" line %d: stepLimit = %d\n", line_no, cfg->stepLimit);
+		}
+		memset(buf, 0x0, LINE_MAX);
+	}
+}
